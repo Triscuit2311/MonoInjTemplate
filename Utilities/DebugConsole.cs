746ab56b7495a3e28ddcb2c52edd4e58b9395ed8
triscuit2311 <dante.trisciuzzi@snhu.edu>
2021-12-20 07:54:36 -0500

    Using new consolebase

--- Utilities/DebugConsole.cs
+++ Utilities/DebugConsole.cs
@@ -1,10 +1,10 @@
 ﻿using System.IO;
 
-namespace MonoInjectionTemplate
+namespace MonoInjectionTemplate.Utilities
 {
-    public class DebugConsole
+    public static class ConsoleBase
     {
-#if DEBUG
+        
         [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
         [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
         private static extern bool AllocConsole();
@@ -12,44 +12,40 @@ namespace MonoInjectionTemplate
         private static extern bool FreeConsole();
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]
         private static extern bool AttachConsole(int dwProcessId);
-        private readonly StreamWriter _stdOutWriter;
-        private readonly StreamReader _stdInReader;
-#endif
+        private static readonly StreamWriter StdOutWriter;
+        private static StreamReader _stdInReader;
 
-        public DebugConsole()
+        static ConsoleBase()
         {
-#if DEBUG
             AllocConsole();
-            Stream stdout = System.Console.OpenStandardOutput();
-            Stream stdin = System.Console.OpenStandardInput();
-            _stdOutWriter = new StreamWriter(stdout);
-            _stdInReader = new StreamReader(stdin);
-            _stdOutWriter.AutoFlush = true;
+            StdOutWriter = new StreamWriter(System.Console.OpenStandardOutput());
+            _stdInReader = new StreamReader(System.Console.OpenStandardInput());
+            StdOutWriter.AutoFlush = true;
 
             AttachConsole(-1);
             WriteLine("Console Initialized");
-#endif
         }
+        
 
-        public void Release()
+        public static void Release()
         {
-#if DEBUG
             FreeConsole();
-#endif
         }
 
-        public string GetLine()
+        public static string GetLine()
         {
-#if DEBUG
             return _stdInReader.ReadLine();
-#endif
         }
-        public void WriteLine(string line)
+        public static void WriteLine(string line)
         {
-#if DEBUG
-            _stdOutWriter.WriteLine(line);
+            StdOutWriter.WriteLine(line);
             System.Console.WriteLine(line);
-#endif
+        }
+
+        public static void Write(string msg)
+        {
+            StdOutWriter.Write(msg);
+            System.Console.Write(msg);
         }
 
     }
